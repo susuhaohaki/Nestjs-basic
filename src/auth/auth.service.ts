3cb0a6bd9638472ab12802d1f0880768da117850
susuhaohaki <nguyenhai250203@gmail.gmailcom>
2024-07-08 16:43:44 +0700

    #36.2 Local Strategies với NestJS (Part 2)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOneByUserName(username);
+    if (user) {
+      const isValid = await this.usersService.isValidPassword(
+        pass,
+        user.password,
+      );
+      if (isValid) {
+        return user;
+      }
+    }
+    return null;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,24 +4,25 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
-import {genSaltSync,hashSync} from 'bcryptjs';
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
 
-  hashPassword = (password : string) => {
+  hashPassword = (password: string) => {
     const salt = genSaltSync(10);
     const hash = hashSync(password, salt);
-    return hash
-  }
-
+    return hash;
+  };
 
   async create(createUserDto: CreateUserDto) {
-    const hashPassword = this.hashPassword(createUserDto.password)
-    let user = await this.UserModel.create({email:createUserDto.email,
-      password : hashPassword,name: createUserDto.name})
-    return user
-
+    const hashPassword = this.hashPassword(createUserDto.password);
+    let user = await this.UserModel.create({
+      email: createUserDto.email,
+      password: hashPassword,
+      name: createUserDto.name,
+    });
+    return user;
   }
 
   findAll() {
@@ -29,20 +30,33 @@ export class UsersService {
   }
 
   findOne(id: string) {
-    if (!mongoose.Types.ObjectId.isValid(id))
-      return "user not found"
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'user not found';
     return this.UserModel.findOne({
-      _id:id
-    })
-  }
-
-  update(updateUserDto: UpdateUserDto) {
-    return this.UserModel.updateOne({_id:updateUserDto._id}, {
-      ...updateUserDto
+      _id: id,
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findOneByUserName(username: string) {
+    return this.UserModel.findOne({
+      email: username,
+    });
+  }
+
+  isValidPassword(password: string, hashPassword: string) {
+    return compareSync(password, hashPassword);
+  }
+
+  update(updateUserDto: UpdateUserDto) {
+    return this.UserModel.updateOne(
+      { _id: updateUserDto._id },
+      {
+        ...updateUserDto,
+      },
+    );
+  }
+
+  remove(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'user not found';
+    return this.UserModel.deleteOne({ _id: id });
   }
 }
